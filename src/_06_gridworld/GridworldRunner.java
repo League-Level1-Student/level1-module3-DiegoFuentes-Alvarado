ce2dfb02c558fca94b1616b88757329f4680743e
Christopher Tablas <Christopher.Tablasdelacruz@gompersstudent.org>
2019-12-09 14:05:13 -0800

    Gridworld
    CowTimer

--- src/_06_gridworld/GridworldRunner.java
+++ src/_06_gridworld/GridworldRunner.java
@@ -1,5 +1,63 @@
 package _06_gridworld;
 
+import java.awt.Color;
+import java.util.Random;
+
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
 public class GridworldRunner {
+public static void main(String[] args) {
+//	World w = new World();
+//	w.show();
+//	
+//	Bug bee = new Bug();
+//	bee.setColor(Color.YELLOW);
+//	Location b = new Location(0, 3);
+//	w.add(b, bee);
+//	
+//	Random random = new Random();
+//	int x = random.nextInt(10);
+//	int y = random.nextInt(10);
+//	Bug ran = new Bug();
+//	ran.setColor(Color.BLUE);
+//	Location r = new Location(x, y);
+//	w.add(r, ran);
+//	ran.turn();
+//	ran.turn();
+//	
+//	Flower f = new Flower();
+//	Location flo = new Location(x-1, y);
+//	w.add(flo, f);
+//	
+//	Flower f2 = new Flower();
+//	Location flo2 = new Location(x+1, y);
+//	w.add(flo2, f2);
+	
+	
+	World america = new World();
+	
+		for(int y = 0; y < 10; y+=2){
+				for(int x = 0; x<10; x++){
+					Flower f = new Flower();
+					f.setColor(Color.WHITE);
+					Location l = new Location(y, x);
+					america.add(l, f);
+			}
+		}
+		for(int y = 1; y < 10; y+=2){
+			for(int x = 0; x<10; x++){
+				Flower f = new Flower();
+				f.setColor(Color.RED);
+				Location l = new Location(y, x);
+				america.add(l, f);
+			}
+		}
+		america.show();
+	}
 
 }
+
+
--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -11,11 +11,20 @@ import javax.swing.JApplet;
 public class CowTimer {
 
 	/* 1. Make a constructor for the CowTimer class that initializes the minutes variable */
-	
+	CowTimer(int m){
+		minutes = m;
+		setTime(m);
+		try {
+			start();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	/* 4. Complete the main method of the CowTimerRunner class */
-
+	
 	private int minutes;
-
+	
 	public void setTime(int minutes) {
 		this.minutes = minutes;
 		System.out.println("Cow time set to " + minutes + " minutes.");
@@ -25,13 +34,18 @@ public class CowTimer {
 		/*
 		 * 2. Count down the minutes, print the current minute then sleep for the number of minutes
 		 * using Thread.sleep(int milliseconds). 
-		 */
-
+		 */while(minutes > 0){
+			 System.out.println(minutes);
+			 minutes--;
+			 Thread.sleep(minutes);
+			
+		 }
+		 
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
 		 * from freesound.org, then drag it intothe default package.
-		 */
+		 */playSound("moo.wav");
 
 	}
 
